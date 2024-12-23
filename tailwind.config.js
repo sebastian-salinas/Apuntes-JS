5b7e9898a7fae10b91e30aaff7f8764348237ed4
sebastian-salinas <sseba67@gmail.com>
2024-12-23 20:12:53 +0100

    Creamos repositorio

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // Ajusta esto según tu estructura de archivos
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
