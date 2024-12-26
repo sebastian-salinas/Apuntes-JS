b156b34e9ff2e841f843b146662e37996499d25c
sebastian-salinas <sseba67@gmail.com>
2024-12-26 11:26:59 +0100

    Añadimos funcion toggle en sidebar

--- src/script.js
+++ src/script.js
@@ -1,4 +1,12 @@
 // Inicializar Highlight.js
 document.addEventListener("DOMContentLoaded", function () {
   hljs.highlightAll(); // Aplica resaltado a todos los bloques de código
+
+  // Eventos de navegación
+  const sidebar = document.getElementById("sidebar");
+  const sidebarOpen = document.getElementById("open-sidebar");
+
+  sidebarOpen.addEventListener("click", () => {
+    sidebar.classList.toggle("hidden");
+  });
 });
