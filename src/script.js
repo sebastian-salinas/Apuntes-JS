52e0b582fcc4e24fa8a8809170f629673329c9de
sebastian-salinas <sseba67@gmail.com>
2025-03-31 22:38:26 +0200

    Actualizamos header en todas las paginas

--- src/script.js
+++ src/script.js
@@ -10,48 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
     sidebar.classList.toggle("hidden");
   });
 
-  document.getElementById("darkModeToggle").addEventListener("click", () => {
+  document.querySelector(".darkModeToggle").addEventListener("click", () => {
     document.documentElement.classList.toggle("dark");
   });
-
-  document.getElementById("menu-toggle").addEventListener("click", function () {
-    document.getElementById("mobile-menu").classList.toggle("hidden");
-  });
-});
-
-const temario = {
-  name: "Introducción",
-  link: "/src/01-index.html",
-  name: "Variables",
-  link: "/src/02-variables.html",
-  name: "Strings",
-  link: "/src/03-strings.html",
-  name: "Numbers",
-  link: "/src/04-numbers.html",
-  name: "Operadores",
-  link: "/src/05-operators.html",
-  name: "Booleans",
-  link: "/src/06-booleans.html",
-  name: "Objetos",
-  link: "/src/07-objects.html",
-  name: "Arrays",
-  link: "/src/08-arrays.html",
-  name: "Arrays Methods",
-  link: "/src/09-arrays-methods.html",
-  name: "Estructuras de control",
-  link: "/src/10-structures.html",
-  name: "Iteradores",
-  link: "/src/11-iterators.html",
-  name: "Funciones",
-  link: "/src/12-functions.html",
-  name: "DOM",
-  link: "/src/13-dom.html",
-  name: "Eventos",
-  link: "/src/14-events.html",
-};
-
-const inputSearch = document.getElementById("search");
-
-inputSearch.addEventListener("change", () => {
-  console.log(inputSearch.value);
 });
